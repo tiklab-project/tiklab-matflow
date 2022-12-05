0163466b07aebd5d12664e5c975da64861310bc1
zhangcheng <zcamyy@126.com>
2022-12-05 20:53:29 +0800

    触发器优化

---
+++ tiklab-matflow-api/src/main/java/net/tiklab/matflow/execute/service/PipelineExecTaskService.java
@@ -0,0 +1,14 @@
+package net.tiklab.matflow.execute.service;
+
+import net.tiklab.matflow.definition.model.PipelineAfterConfig;
+import net.tiklab.matflow.definition.model.PipelineCourseConfig;
+import net.tiklab.matflow.execute.model.PipelineProcess;
+
+public interface PipelineExecTaskService {
+
+
+    boolean beginCourseState(PipelineProcess pipelineProcess, PipelineCourseConfig config);
+
+    boolean beginAfterState(PipelineProcess pipelineProcess, PipelineAfterConfig config);
+
+}
---
+++ tiklab-matflow-server/src/main/java/net/tiklab/matflow/execute/service/PipelineExecTaskServiceImpl.java
@@ -0,0 +1,84 @@
+package net.tiklab.matflow.execute.service;
+
+import net.tiklab.matflow.definition.model.PipelineAfterConfig;
+import net.tiklab.matflow.definition.model.PipelineCourseConfig;
+import net.tiklab.matflow.execute.model.PipelineProcess;
+import net.tiklab.matflow.execute.service.achieve.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PipelineExecTaskServiceImpl implements PipelineExecTaskService {
+
+    @Autowired
+    CodeService code ;
+
+    @Autowired
+    BuildService build ;
+
+    @Autowired
+    TestService test;
+
+    @Autowired
+    DeployService deploy ;
+
+    @Autowired
+    CodeScanService codeScan;
+
+    @Autowired
+    ProductService product;
+
+    @Autowired
+    MessageExecService message;
+
+    public boolean beginCourseState(PipelineProcess pipelineProcess, PipelineCourseConfig config){
+        int taskType = config.getTaskType();
+        boolean state = true;
+        switch (taskType/10) {
+            case 0 -> state = code.clone(pipelineProcess, config);
+            case 1 -> state = test.test(pipelineProcess, config);
+            case 2 -> state = build.build(pipelineProcess, config);
+            case 3 -> state = deploy.deploy(pipelineProcess, config);
+            case 4 -> state = codeScan.codeScan(pipelineProcess, config);
+            case 5 -> state = product.product(pipelineProcess,  config);
+        }
+        return state;
+    }
+
+
+    public boolean beginAfterState(PipelineProcess pipelineProcess, PipelineAfterConfig config){
+       return message.message(pipelineProcess, config.getConfigId());
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
